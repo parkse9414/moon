76c442f5e8c5d672b6d5650ceb256705bf0f9465
박세원 <parkse9414@naver.com>
2014-11-06 15:46:09 +0900

    ddddd

--- sketch.js
+++ sketch.js
@@ -2,40 +2,41 @@ var img;
 
 function setup()
 {
-    createCanvas(900,900);
+    createCanvas(1000,1000);
     img = loadImage("1Mercury.png");  
     img2 = loadImage("2Venus.png");
     img3 = loadImage("3earth.png");
     img4 = loadImage("4moon.png");
+    img5 = loadImage("bg.png")
 }
 
 function draw()
 {
-	background(255);
+	background(img5);
     fill(237,112,50);
     noStroke();
-    ellipse(450,450,350,350);
+    ellipse(500,500,350,350);
 
 	// 수성
     var duration = 5000;
     var timing = (new Date()%duration)/duration;
-	image(img, 450 + Math.cos(timing*2*PI)*200 - 9, 450 + Math.sin(timing*2*PI)*200 - 9);
+	image(img, 500 + Math.cos(timing*2*PI)*200 - 9, 500 + Math.sin(timing*2*PI)*200 - 9);
 
 	// 금성
     var duration = 4000;
     var timing = (new Date()%duration)/duration;
-    image(img2, 450 + Math.cos(timing*2*PI)*250 - 27, 450 + Math.sin(timing*2*PI)*250 - 27);
+    image(img2, 500 + Math.cos(timing*2*PI)*250 - 27, 500 + Math.sin(timing*2*PI)*250 - 27);
 
     //지구
     var duration = 4500;
     var timing = (new Date()%duration)/duration;
-    image(img3, 450 + Math.cos(timing*2*PI)*350 - 32, 450 + Math.sin(timing*2*PI)*350 - 32);
+    image(img3, 500 + Math.cos(timing*2*PI)*350 - 32, 500 + Math.sin(timing*2*PI)*350 - 32);
 
     //달
     var duration = 4500;
     var timing = (new Date()%duration)/duration;
-    image(img4, 450 + Math.cos(timing*2*PI)*350 + Math.cos(timing*4*PI)*80 - 8, 
-    450 + Math.sin(timing*2*PI)*350 + Math.sin(timing*4*PI)*80 - 8)
+    image(img4, 500 + Math.cos(timing*2*PI)*350 + Math.cos(timing*4*PI)*80 - 8, 
+    500 + Math.sin(timing*2*PI)*350 + Math.sin(timing*4*PI)*80 - 8)
 
 
 
