45b80a95307ab72e722afef2f351f98dc96918fc
박세원 <parkse9414@naver.com>
2014-11-06 14:39:50 +0900

    ddddd

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ var img;
 function setup()
 {
     createCanvas(900,900);
-    img = loadImage("1Mercury.png");  // Load the image 
+    img = loadImage("1Mercury.png");  
+    img2 = loadImage("2Venus.png");
 }
 
 function draw()
@@ -18,8 +19,10 @@ function draw()
     var timing = (new Date()%duration)/duration;
 
 
-	// Displays the image at its actual size at point (0,0)
+	// 수성
 	image(img, 450 + Math.cos(timing*2*PI)*200 - 17, 450 + Math.sin(timing*2*PI)*200 - 17);
+	// 금성
+    image(img, 450 + Math.cos(timing*2*PI)*250 - 54, 450 + Math.sin(timing*2*PI)*250 - 54);
 
     //지구
 	fill(0);
@@ -33,22 +36,7 @@ function draw()
     450 + Math.sin(timing*2*PI)*350 + Math.sin(timing*4*PI)*80,
     30, 30);  
 
-    //수성
-    var duration = 7000;
-    var timing = (new Date()%duration)/duration;
 
-    fill(197,182,182);
-    ellipse(450 + Math.cos(timing*2*PI)*200,                       
-    450 + Math.sin(timing*2*PI)*200,
-    10, 10);  
 
-    //금성
-    var duration = 7600;
-    var timing = (new Date()%duration)/duration;
-
-    fill(169,127,93);
-    ellipse(450 + Math.cos(timing*2*PI)*250,                       
-    450 + Math.sin(timing*2*PI)*250,
-    50, 50);  
 
 }
